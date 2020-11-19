416481cdb6dfe41bf8914e538eb17f378b49a196
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-19 09:52:32 -0500

    create gesture

--- DragGesture/ContentView.swift
+++ DragGesture/ContentView.swift
@@ -14,11 +14,17 @@ struct ContentView: View {
     @State private var newPosition: CGSize = .zero
     
     var body: some View {
-                Circle()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(Color.red)
-                    .offset(x: self.currentPosition.width, y: self.currentPosition.height)
-    }
+        // Create Circle
+        Circle()
+            .frame(width: 100, height: 100)
+            .foregroundColor(Color.red)
+            .offset(x: self.currentPosition.width, y: self.currentPosition.height)
+            // Create Circle
+            .gesture(DragGesture()
+                        .onChanged { value in
+                            self.currentPosition = CGSize(width: value.translation.width + self.newPosition.width, height: value.translation.height + self.newPosition.height)
+                        }
+            }
 }
 
 struct ContentView_Previews: PreviewProvider {
