36e67e76637380c9f0ba8d59e21e0e9c17d6f7c1
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-19 09:49:59 -0500

    Initial Commit

---
+++ DragGesture/DragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureApp.swift
+//  DragGesture
+//
+//  Created by Cloutier, Vincent on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
